3766e270d2bd231882bb97f6ac9cee3d45b06702
Corina Pelichowski <corina@danitt.com>
2019-11-16 20:26:28 +1000

    addition of about and contact  files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 export default () => (
-  <div>
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
     <p>
       What do I like to do? Lots of course but definitely enjoy building
@@ -8,5 +8,3 @@ export default () => (
     </p>
   </div>
 )
-
-export default () => <div>Hello world!</div>
